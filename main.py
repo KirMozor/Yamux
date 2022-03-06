2456e73e6cf3726d3fd3b5f39a8b8604cff884c9
KirMozor <kirmozor@gmail.com>
2022-03-06 22:37:51 +0600

    Changed authorization by token I had a deprecated method

--- main.py
+++ main.py
@@ -20,15 +20,19 @@ except FileNotFoundError:
         file = open("config.toml", "w")
         file.write('tokenYandex = ""')
         file.close()
-        
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         def __init__(self):
+                logText = ""
                 # Это здесь нужно для доступа к переменным, методам
                 # и т.д. в файле design.py
                 super().__init__()
+                logText += "\nИнициализация интерфейса"
                 self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+                self.showLog.setText(logText)
 
                 # Проверяем на рабочий токен и то что интернет работает
+                logText += "\nПроверяем на рабочий токен и на рабочий интернет"
+                self.showLog.setText(logText)
                 try:
                         if not config.get('tokenYandex'):
                                 text, ok = QInputDialog.getText(self, 'Добавить токен', 'Я не обнаружил токена YandexMusic в программе. Если у вас нет токена то инструкция по получению токена находится в README.md')
@@ -37,26 +41,40 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                                         file.write(f'tokenYandex = "{text}"')
                                         file.close()
                         global client 
-                        client = Client.from_token(config.get('tokenYandex'))
+                        client = Client(config.get('tokenYandex')).init()
                 except yandex_music.exceptions.NetworkError:
-                        self.errorStandart("Проблемы с интернетом", "Yandex Music Api не видит вашего интернета, проверь, всё ли с ним в порядке", exitOrNo=True)
+                        logText += "\nПроблемы с интернетом yandex_music.exceptions.NetworkError"
+                        self.showLog.setText(logText)
+                        self.errorStandart("Проблемы с интернетом",
+                                "Yandex Music Api не видит вашего интернета, проверь, всё ли с ним в порядке",
+                                exitOrNo=True)
                 except yandex_music.exceptions.Unauthorized:
+                        logText += "\nНеправильный токен yandex_music.exceptions.Unauthorized"
+                        self.showLog.setText(logText)
                         self.errorConfig("Неправильный токен",
                                 "Yandex Music Api говорит что у вас нерабочий токен. Инструкция по получению токена находится в README.md",
                                 windowsTitle="Добавить токен")
                 except UnicodeEncodeError:
+                        logText += "\nТокен из непонятных символов UnicodeEncodeError"
+                        self.showLog.setText(logText)
                         self.errorConfig("Токен из непонятных символов", 
                                 "YandexMusic говорит что у вас токен из непонятных символов, ведите корректный токен сюда. Инструкция по получению токена находится в README.md",
                                 windowsTitle="Добавить токен")
                 except NameError:
+                        logText += "\nТолько что создался конфиг, перезапустите программу NameError"
+                        self.showLog.setText(logText)
                         self.errorConfig("Только что создался конфиг, перезапустите программу",
                                 "У вас нету файла с настройками программы, я его создам, но мне нужно знать токен от YandexMusic. Инструкция по получению токена находится в README.md",
                                 windowsTitle="Добавить токен")
                 self.pushButtonToPlay.clicked.connect(self.enterLinkToPlay)
                 self.pushButtonToDownload.clicked.connect(self.enterLinkToDownload)
         def exit(self):
+                logText += "\nЗапускаю функцию exit"
+                self.showLog.setText(logText)
                 sys.exit()
         def playLocalFile(self):
+                logText += "\nЗапустился playLocalFile"
+                self.showLog.setText(logText)
                 wb_patch = QtWidgets.QFileDialog.getOpenFileName()[0]
                 locale.setlocale(locale.LC_NUMERIC, 'C')
                 player = mpv.MPV()
