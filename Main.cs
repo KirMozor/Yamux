524a3cfb263b320dca2b41b1581f5ebe3babb462
KirMozor <kirmozor96@gmail.com>
2022-06-02 18:01:15 +0600

    Выровнил полоску прокрутки трека. Удалил не нужную ветку, подправил README.md

---
+++ Main.cs
@@ -0,0 +1,23 @@
+using System;
+using Application = Gtk.Application;
+
+namespace Yamux
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application.Init();
+
+            Application app = new Application("org.KirMozor.Yamux", GLib.ApplicationFlags.None);
+            app.Register(GLib.Cancellable.Current);
+
+            LoginWindow win = new LoginWindow();
+            app.AddWindow(win);
+
+            win.Show();
+            Application.Run();
+        }
+    }
+}
