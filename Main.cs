47290a90b3b2e21e52f567c5ce4ab7220cf6eea6
KirMozor <kirmozor96@gmail.com>
2022-06-22 20:18:08 +0600

    Заменил все ссылки для скачивания иконок на GitHub ссылки

--- Main.cs
+++ Main.cs
@@ -14,18 +14,18 @@ namespace Yamux
                 Directory.CreateDirectory("Svg");
                 List<Uri> IconList = new List<Uri>()
                 {
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1lD--PBhOuFf1-MbhNu0dC6l6C9U-t4Xe"), //about_icon.svg
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1-afcpIaXaBc7kEesJ4HHXiAjpVvEwJ_I"), //icon.svg
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1AznTfYXfcSeXHkYYXP4ObCd3QHc7gm6F"), //icons8_rock_music_100_negate.png
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1RD8GVXEHgLIsrjJjadozfrjv814vJoeI"), //icons8-download.png
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=19ThmVFjOfPuTRtUx63BBNcMlIVmDhKsY"), //icons8-next.png
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1xzpRFD1Pm7q3LY8i_lOHliOGBtVQBaM4"), //icons8-pause.png
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=14-dOuiZzJSlWxG_ueFcKfAi8g4ghJ3ey"), //icons8-play.png
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1IoXzhgrOIyimcGJxzZYe6Nq6W2e3HgvL"), //icons8-previous.png
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1w5jcd4h8lL8lWsxHdRkxMafDdYOqMmY2"), //icons8-settings-20.png
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1ZQRsi_Il5cyM1P1lSZbyA1egwXCw4MKx"), //icons8-stop.png
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1cZccppheNu60uNeLfNatIsh-AtbA01AJ"), //yandex_en_icon-icons.com_61632(1).png
-                    new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1QGFY6HVE1nz8l2hqfoHOCQOetAJghn1H"), //icons8_rock_music_100_negate50x50.png
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/about_icon.svg"), //about_icon.svg
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/icon.svg"), //icon.svg
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/icons8_rock_music_100_negate.png"), //icons8_rock_music_100_negate.png
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/icons8-download.png"), //icons8-download.png
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/icons8-next.png"), //icons8-next.png
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/icons8-pause.png"), //icons8-pause.png
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/icons8-play.png"), //icons8-play.png
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/icons8-previous.png"), //icons8-previous.png
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/icons8-settings-20.png"), //icons8-settings-20.png
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/icons8-stop.png"), //icons8-stop.png
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/yandex_en_icon-icons.com_61632(1).png"), //yandex_en_icon-icons.com_61632(1).png
+                    new Uri("https://raw.githubusercontent.com/KirMozor/Yamux/main/Svg/icons8_rock_music_100_negate50x50.png"), //icons8_rock_music_100_negate50x50.png
                 };
                 Console.WriteLine("Icons are downloading\nPlease wait");
                 Player.DownloadWithSync(IconList[0], "Svg/about_icon.svg");
